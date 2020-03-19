039e4f5f91dfc6bba4521109d0a180f0f1625bb0
Никита Колчанов <sikmakn@gmail.ru>
2020-03-19 22:21:56 +0300

    start create site with exercices for programming

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const jsRouter = require('./back/controllers/jsController');
+
+const app = express();
+
+const hbs = exphbs.create({
+    // defaultLayout: 'main',
+    extname: 'hbs',
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './front/views');
+
+app.use(express.static(__dirname + '/front/static'));
+
+app.use("/js", jsRouter);
+app.get('/', (req, res) => {
+    res.render('layouts/main.hbs')
+});
+
+
+app.get('/net', (req, res) => {
+    res.render('empty.hbs', {layout: 'themeSelectMain.hbs', isNet: true})
+});
+
+app.get('/python', (req, res) => {
+    res.render('empty.hbs', {layout: 'themeSelectMain.hbs', isPython: true})
+});
+
+app.listen(3001);
