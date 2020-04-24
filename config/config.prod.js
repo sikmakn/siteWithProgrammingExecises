d683d8adca6a39c721dfb96f682fc01e8334fdf4
Никита Колчанов <sikmakn@gmail.ru>
2020-04-25 01:06:08 +0300

    change env for config

--- config/config.prod.js
+++ config/config.prod.js
@@ -3,11 +3,11 @@ const {
     MONGO_HOST,
     MONGO_USERNAME,
     MONGO_PASSWORD,
-    MONGO_DB_NAME
+    MONGO_DB_NAME,
 } = process.env;
 
 module.exports = {
-    MONGODB_URI: `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_PORT}:${MONGO_HOST}/${MONGO_DB_NAME}?authSource=admin`,
+    MONGODB_URI: `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}?authSource=admin`,
     ADMIN_BEARER: process.env.ADMIN_BEARER,
     COMPILER_URI: process.env.COMPILER_URI,
 };
